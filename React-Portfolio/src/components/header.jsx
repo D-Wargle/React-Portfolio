b0f34bb399488357fcf262861ee9b64793c13fe2
Daniel T <Danieltwarg@gmail.com>
2024-11-14 16:01:32 -0500

    set starter code for components

---
+++ React-Portfolio/src/components/header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <div>
+                    <h1></h1>
+                    <button></button>
+                </div>
+            </nav>
+        </header>
+    )
+}
