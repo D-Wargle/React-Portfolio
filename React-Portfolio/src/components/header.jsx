20ba5005161a59d0152e1c1724c76573afb28274
Daniel T <Danieltwarg@gmail.com>
2024-11-14 17:50:12 -0500

    started working on header.jsx

--- React-Portfolio/src/components/header.jsx
+++ React-Portfolio/src/components/header.jsx
@@ -1,10 +1,16 @@
 export default function Header() {
     return (
+        {/*header element for index file*/}
         <header>
-            <nav>
-                <div>
-                    <h1></h1>
-                    <button></button>
+            {/*nav element for header*/}
+            <nav className="">
+                <div className="">
+                    {/*h1 element for header*/}
+                    <h1 className="">Daniel Wargo</h1>
+                    {/*button element to toggle for mobile*/}
+                    <button className="" type="button">
+                        <span className=""></span>
+                    </button>
                 </div>
             </nav>
         </header>
